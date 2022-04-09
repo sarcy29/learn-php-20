8be33d8a3f50f9c7773ea2b56991d422b8f57d1d
sarcy  nenepat <82246009+sarcy29@users.noreply.github.com>
2022-04-09 11:06:52 +0900

    add php20

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+        //loop
+
+        //while loop
+        //do while loop
+        //for loop
+        //foreach loop
+        $array = array("Daniel", "Jane", "Jacob", "Shania", "Jimmy");
+
+        foreach ($array as $loopdata) {
+            echo "My name" .$loopdata. "<br>";
+        }
+    ?>
+
+    </body>
+    </html>
